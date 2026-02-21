2d400e074ab7aa9ceb2871778ae40a0273a9baa4
Abdul Basit Bhatti <78906762+basitbhatti@users.noreply.github.com>
2026-02-21 15:50:20 +0500

    Filled OTP Text Field Done

--- app/src/main/java/com/basitbhatti/compose/library/ui/screens/OTPScreen.kt
+++ app/src/main/java/com/basitbhatti/compose/library/ui/screens/OTPScreen.kt
@@ -1,15 +1,20 @@
 package com.basitbhatti.compose.library.ui.screens
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -19,7 +24,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.basitbhatti.compose.ui_kit.otp.OTPTextFieldFilled
 
 @Composable
 fun OTPScreen(
@@ -31,13 +38,14 @@ fun OTPScreen(
 
     var otpText by remember { mutableStateOf("") }
 
-    val maxLength by remember { mutableStateOf(4) }
+    var maxLength by remember { mutableStateOf(4) }
 
     Column(
-        modifier = modifier.fillMaxSize()
+        modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
         Row(
+            modifier = Modifier.fillMaxWidth(),
             verticalAlignment = Alignment.CenterVertically
         ) {
             IconButton(
@@ -55,6 +63,49 @@ fun OTPScreen(
 
         }
 
+        Text("Filled OTP View Example", style = MaterialTheme.typography.bodyMedium)
+
+        OTPTextFieldFilled(
+            otpText = otpText,
+            maxLength = maxLength,
+        ) { txt, isCompleted ->
+            otpText = txt
+        }
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalArrangement = Arrangement.Center,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+
+            RadioButton(
+                selected = maxLength == 4, onClick = {
+                    if (maxLength != 4){
+                        maxLength = 4
+                        otpText = ""
+                    }
+                })
+
+            Spacer(modifier = Modifier.width(5.dp))
+
+            Text("4 Digits")
+
+            Spacer(modifier = Modifier.width(25.dp))
+
+            RadioButton(
+                selected = maxLength == 6, onClick = {
+                    if (maxLength != 6) {
+                        maxLength = 6
+                        otpText = ""
+                    }
+                })
+
+            Spacer(modifier = Modifier.width(5.dp))
+
+            Text("6 Digits")
+
+
+        }
 
 
     }
--- ui-kit/src/main/java/com/basitbhatti/compose/ui_kit/otp/OTPFilled.kt
+++ ui-kit/src/main/java/com/basitbhatti/compose/ui_kit/otp/OTPFilled.kt
@@ -1,17 +1,29 @@
 package com.basitbhatti.compose.ui_kit.otp
 
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -21,6 +33,9 @@ import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,22 +44,29 @@ import androidx.compose.ui.unit.dp
 fun OTPTextFieldFilled(
     otpText: String,
     maxLength: Int,
-    backgroundColor: Color = Color.Gray,
+    backgroundColor: Color = Color.LightGray,
     onOtpTextChange: (String, Boolean) -> Unit
 ) {
 
     val focusRequester = remember { FocusRequester() }
 
-    Box(Modifier.fillMaxWidth()) {
+    val keyboardController = LocalSoftwareKeyboardController.current
+
+    Box(Modifier.fillMaxWidth(), contentAlignment = Alignment.Center) {
 
         BasicTextField(
             value = otpText,
             onValueChange = {
                 if (it.length <= maxLength) {
                     onOtpTextChange(it, it.length == maxLength)
+                    if (it.length == maxLength) {
+                        keyboardController?.hide()
+                    }
                 }
             },
-            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword),
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.NumberPassword, imeAction = ImeAction.Done
+            ),
             modifier = Modifier
                 .fillMaxWidth()
                 .focusRequester(focusRequester)
@@ -70,8 +92,7 @@ fun OTPTextFieldFilled(
             }
         }
 
-
-        LaunchedEffect(Unit) {
+        LaunchedEffect(maxLength) {
             focusRequester.requestFocus()
         }
 
@@ -85,13 +106,42 @@ fun OTPTextFieldFilled(
 fun OTPCell(
     char: String, isFocused: Boolean, backgroundColor: Color, modifier: Modifier = Modifier
 ) {
+
+    val infiniteTransition = rememberInfiniteTransition()
+    val cursorAlpha by infiniteTransition.animateFloat(
+        initialValue = 0f, targetValue = 1f, animationSpec = infiniteRepeatable(
+            animation = tween(500), repeatMode = RepeatMode.Reverse
+        )
+    )
+
+    val border = BorderStroke(
+        1.dp, color = if (isFocused) {
+            Color.Black
+        } else {
+            Color.Transparent
+        }
+    )
+
     Box(
         modifier = modifier
             .size(50.dp)
             .clip(RoundedCornerShape(10.dp))
-            .background(backgroundColor),
+            .background(backgroundColor)
+            .border(border = border, shape = RoundedCornerShape(10.dp)),
         contentAlignment = Alignment.Center
     ) {
+        if (char.isNotEmpty()) {
+            Text(text = char, style = MaterialTheme.typography.bodyMedium.copy(
+                fontWeight = FontWeight.Bold
+            ))
+        } else if (isFocused) {
+            Box(
+                modifier = Modifier
+                    .width(1.dp)
+                    .height(15.dp)
+                    .background(Color.Black.copy(alpha = cursorAlpha))
+            )
+        }
 
     }
 
